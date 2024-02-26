3df80dc96a576678d64e17e67a21e1f1aae0592a
charlescleyton <cleyton.master@gmail.com>
2024-02-26 12:20:59 -0300

    chore: alteração contrato do projeto

--- Readme.md
+++ Readme.md
@@ -33,6 +33,3 @@ Fiz esse projeto quando estava estudando aplicação node.js, usei um dos meus l
 3. crie um um arquivo .env com o database acces do mongodb
 4. de npm install
 5. rode o projeto com o comando 'npm run dev'
-6. e para ver a magica acontecer faca a requisição no postman ou no insominia com o seguinte body:
-
-   `{    "title": "Episódio V – O Império Contra-Ataca",   "description": "Três anos após a destruição da Estrela da Morte, a Aliança Rebelde continua fugindo da ameaça do Império Galáctico, que manda sondas espiãs por toda a Galáxia em busca dos rebeldes. Uma das sondas vai para o planeta gelado de Hoth, onde a Aliança Rebelde montou a base Echo. Han Solo planeja partir para pagar o dinheiro que deve ao gangster Jabba the Hutt, embora tenha sentimentos pela Princesa Leia, que nega gostar dele.",     	  "image_url": "https://upload.wikimedia.org/wikipedia/pt/5/5c/The_Empire_Strikes_Back.jpg",   "trailer_ur": "https://www.youtube.com/watch?v=P8bq_zB201Y",   "genero": "Ficção Cientifica | Syfy"   }`
--- src/index.js
+++ src/index.js
@@ -10,49 +10,56 @@ const dbConnect = process.env.DB_CONNECT;
 mongoose.connect(dbConnect);
 
 const filmeSchema = new mongoose.model('Film', {
-    title: String,
-    description: String,
-    image_url: String,
+    titulo: String,
+    sinopse: String,
+    img_url: String,
     trailer_url: String,
     genero: String,
     ano: Number,
-  });
+    roteiro: String,
+});
 
 app.get('/', async (req, res) => {
-    const fims = await filmeSchema.find();
-    res.send(fims);
+    const films = await filmeSchema.find();
+    if (films.length > 0) {
+        res.send(films);
+    } else {
+        res.send("Não há Filmes cadastrados");
+    }
 });
 
 app.post('/', async (req, res) => {
-    const film = new filmeSchema({ 
-        title: req.body.title,
-        description: req.body.description,
-        image_url: req.body.image_url,
+    const film = new filmeSchema({
+        titulo: req.body.titulo,
+        sinopse: req.body.sinopse,
+        img_url: req.body.img_url,
         trailer_url: req.body.trailer_url,
-        genero: req.body.genero, 
+        genero: req.body.genero,
         ano: req.body.ano,
+        roteiro: req.body.roteiro,
     });
     await film.save();
-    res.send(film);
+    res.send("Filme cadastrado com sucesso");
 })
 
 app.delete('/:id', async (req, res) => {
     const film = await filmeSchema.findByIdAndDelete(req.params.id);
-    res.send(film);
+    res.send(film.title + "\nFilme deletado com sucesso");
 })
 
 app.put('/:id', async (req, res) => {
-    const film = await filmeSchema.findByIdAndUpdate(req.params.id, {
-        title: req.body.title,
-        description: req.body.description,
-        image_url: req.body.image_url,
+    await filmeSchema.findByIdAndUpdate(req.params.id, {
+        titulo: req.body.titulo,
+        sinopse: req.body.sinopse,
+        img_url: req.body.img_url,
         trailer_url: req.body.trailer_url,
         genero: req.body.genero,
         ano: req.body.ano,
+        roteiro: req.body.roteiro,
     });
-    res.send(film);
+    res.send("Filme atualizado com sucesso");
 })
 
 app.listen(port, () => {
     console.log(`Porta funcionando: ${port}`)
-  })
+})
